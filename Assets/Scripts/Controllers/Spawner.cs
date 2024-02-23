a8a2eec6e9c55b473552c59036be300d9b2e1e2e
m-mavrin <m.mavrin@carx-tech.com>
2024-02-23 16:31:28 +0300

    контроллеры перенесены в папку Controllers

---
+++ Assets/Scripts/Controllers/Spawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject m_target;
+    [SerializeField] private GameObject m_monster;
+    [SerializeField] private float m_respawnTime;
+
+    public GameObject SpawnMonster()
+    {
+        GameObject newMonster = Instantiate(m_monster);
+        newMonster.transform.SetPositionAndRotation(transform.position, Quaternion.Euler(0, 90, 0));
+        newMonster.GetComponent<Monster>().moveTarget = m_target;
+
+        return newMonster;
+    }
+}
