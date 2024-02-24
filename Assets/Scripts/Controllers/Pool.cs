d4550566cb3522bc34946971bdcf5e73975f06ce
m-mavrin <m.mavrin@carx-tech.com>
2024-02-24 22:54:42 +0300

    реализация класса Pool для любых объектов

---
+++ Assets/Scripts/Controllers/Pool.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool<T> where T : MonoBehaviour
+{
+    [SerializeField] private T m_prefab { get; }
+    [SerializeField] private Transform m_container { get; }
+
+    private List<T> m_pool;
+    private readonly int m_poolSize = 16;
+
+    public Pool(T prefab, Transform container)
+    {
+        m_prefab = prefab;
+        m_container = container;
+        CreatePool();
+    }
+
+    private void CreatePool()
+    {
+        m_pool = new List<T>(m_poolSize);
+        for (int i = 0; i < m_pool.Count; i++)
+        {
+            CreateObject();
+        }
+    }
+
+    private T CreateObject(bool isActiveByDefault = false)
+    {
+        var createdObject = Object.Instantiate(m_prefab, m_container);
+        createdObject.gameObject.SetActive(isActiveByDefault);
+        m_pool.Add(createdObject);
+        return createdObject;
+    }
+
+    public bool IsHasFreeObject(out T obj)
+    {
+        foreach (var item in m_pool)
+        {
+            if (!item.gameObject.activeInHierarchy)
+            {
+                obj = item;
+                item.gameObject.SetActive(true);
+                return true;
+            }
+        }
+
+        obj = null;
+        return false;
+    }
+
+    public T GetFreeElement()
+    {
+        if (IsHasFreeObject(out var obj))
+        {
+            return obj;
+        }
+
+        return CreateObject(true);
+    }
+}
