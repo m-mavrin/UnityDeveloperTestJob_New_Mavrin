30f9d834d702b0c12b9c6bef21144d8d141ca79a
m-mavrin <m.mavrin@carx-tech.com>
2024-02-24 19:33:12 +0300

    вынос функционала поворота башни в отдельный метод

--- Assets/Scripts/Controllers/CannonTower.cs
+++ Assets/Scripts/Controllers/CannonTower.cs
@@ -15,14 +15,18 @@ public class CannonTower : TowerBase
             }
             else
             {
-                var targetPosition = CalculateLeadPoint(m_target.transform.position, m_target.GetComponent<Rigidbody>().velocity, m_towerData.ProjectileSpeed);
-                var angle = Quaternion.LookRotation(targetPosition - m_shootPoint.position);
-
-                transform.rotation = Quaternion.Lerp(transform.rotation, angle, Time.deltaTime * m_towerData.RotationSpeed);
-
+                Rotate();
                 Shoot(false);
             }
         }
+    }
+
+    private void Rotate()
+    {
+        var targetPosition = CalculateLeadPoint(m_target.transform.position, m_target.GetComponent<Rigidbody>().velocity, m_towerData.ProjectileSpeed);
+        var angle = Quaternion.LookRotation(targetPosition - m_shootPoint.position);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, angle, Time.deltaTime * m_towerData.RotationSpeed);
     }
 
     private Vector3 CalculateLeadPoint(Vector3 targetPosition, Vector3 targetVelocity, float projectileSpeed)
